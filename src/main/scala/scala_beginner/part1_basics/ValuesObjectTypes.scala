de68a18904cd60da58825b4fa3008347633a7fe4
David Liu <davidlzs@gmail.com>
2019-10-07 06:43:58 -0600

    Scala beginner - part1 : Value and Variables and basic types

---
+++ src/main/scala/scala_beginner/part1_basics/ValuesObjectTypes.scala
@@ -0,0 +1,29 @@
+package scala_beginner.part1_basics
+
+object ValuesObjectTypes extends App {
+
+  val x : Int = 23
+  println(x)
+
+  // VALS are immutable
+
+  // TYPE inference
+
+  val name : String = "Hello"
+
+  // semicolon is optional
+
+  val isActive: Boolean = true // or false
+  val ch : Char = 'a'
+  val age: Int = 90
+  val height : Short = 1700
+  val distance: Long = 234567890L
+  val temperature: Float = 12.6F
+  val amount: Double = 29804.03D
+
+  var counter: Int = 4
+  counter = 6
+  // variable is used for side effects
+
+
+}
