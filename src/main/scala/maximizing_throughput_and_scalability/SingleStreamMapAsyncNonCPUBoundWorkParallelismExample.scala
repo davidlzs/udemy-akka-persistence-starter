2876ba3d04c4aecfda7277d5e8350b7fa835cf34
David Liu <david.liu@yardi.com>
2020-04-01 16:36:30 -0600

    Use mapAsync to increase parallelism of stream for NonCPU bound work load

--- src/main/scala/maximizing_throughput_and_scalability/SingleStreamMapAsyncNonCPUBoundWorkParallelismExample.scala
+++ src/main/scala/maximizing_throughput_and_scalability/SingleStreamMapAsyncNonCPUBoundWorkParallelismExample.scala
@@ -4,26 +4,34 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Sink, Source}
 
-import scala.concurrent.Future
-import scala.util.{Failure, Success}
+import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.{Future, Promise}
+import scala.util.{Failure, Random, Success}
+import scala.concurrent.duration._
 
-object SingleStreamMapAsyncParallelismExample extends App {
+object SingleStreamMapAsyncNonCPUBoundWorkParallelismExample extends App {
   implicit val system = ActorSystem("singleStreamSystem")
   implicit val materializer = ActorMaterializer()
   implicit val dispatcher = system.dispatcher
+  val random = new Random()
 
-  // Simulate a CPU - intensive workload that takes ~ 10 milliseconds
-  def spin(value: Int): Int = {
-    val start = System.currentTimeMillis()
-    while((System.currentTimeMillis() - start) < 10) {}
-    value
+  def nonBlocking(value: Int) : Future[Int] = {
+    val promise = Promise[Int]
+
+    // Simulate a non-blocking network call to another service
+    val t = FiniteDuration(random.nextInt(101), MILLISECONDS)
+    system.scheduler.scheduleOnce(t) {
+      promise.success(value)
+    }
+
+    promise.future
   }
 
   val start = System.currentTimeMillis();
   Source(1 to 1000)
     // mapAsync executes a Future and emit the completed results to the downstream
-    .mapAsync(2)(x => Future(spin(x)))
-    .mapAsync(2)(x => Future(spin(x)))
+    .mapAsync(1000)(x => nonBlocking(x))
+    .mapAsync(1000)(x => nonBlocking(x))
     .runWith(Sink.ignore)
     .onComplete {
       case Success(done) => println(s"${done} in milliseconds:  ${System.currentTimeMillis() - start}")
